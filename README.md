72c029e8c3c6a2b7a40d98b758b0403598cf5f8f
Alexander Salamatov <ch1seL@ya.ru>
2020-07-08 14:24:27 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# setup-tools
+
+<p align="left">
+  <a href="https://github.com/ton-actions/setup-tools"><img alt="GitHub Actions status" src="https://github.com/ton-actions/setup-tools/workflows/Main%20workflow/badge.svg"></a>
+  <a href="https://github.com/ton-actions/build-solc"><img alt="GitHub Actions status" src="https://github.com/ton-actions/build-solc/workflows/Build%20and%20release%20solidity%20compiler/badge.svg"></a>
+  <a href="https://github.com/ton-actions/build-tvm-linker"><img alt="GitHub Actions status" src="https://github.com/ton-actions/build-tvm-linker/workflows/Build%20and%20release%20TVM-Linker/badge.svg"></a>
+</p>
+
+This action sets up a tonos cli tools for use in actions.
+Avalible tool list:
+
+- [tonos-cli](https://github.com/tonlabs/tonos-cli) 
+- [solc](https://github.com/tonlabs/TON-Solidity-Compiler) 
+- [tvm_linker](https://github.com/tonlabs/TVM-linker) 
+- env variable TVM_LINKER_LIB_PATH set to stdlib_sol.tvm
+
+# Usage
+
+See [action.yml](action.yml)
+
+```yaml
+steps:
+- uses: actions/checkout@main
+- uses: ton-actions/setup-tools@v1
+- run: |
+  tonos-cli --help
+  solc --help
+  tvm_linker --help
+```
+
+# Don't forget to tip
+
+My Free TON address: 
+0:9b487d68e4f029ab6d92640892d99d1c549ae69b198df414e905350559a165bf
+
+https://ton.surf
---
+++ index.js
@@ -0,0 +1,91 @@
+const github = require('@actions/github');
+const core = require('@actions/core');
+const tc = require('@actions/tool-cache');
+
+const gitHubToken = core.getInput('gitHubToken');
+const octokit = github.getOctokit(gitHubToken)
+
+const toolsDir = 'tools';
+
+const tools = [
+    {
+        name: 'solc',
+        repoOwner: 'ton-actions',
+        repoName: 'build-solc',
+        assetPrefix: 'solc',
+        addToPath: true
+    },
+    {
+        name: 'stdlib_sol.tvm',
+        repoOwner: 'ton-actions',
+        repoName: 'build-solc',
+        assetPrefix: 'stdlib_sol.tvm',
+        addEnvVariable: 'TVM_LINKER_LIB_PATH'
+    },
+    {
+        name: 'tonos-cli',
+        repoOwner: 'tonlabs',
+        repoName: 'tonos-cli',
+        assetPrefix: 'tonos-cli',
+        addToPath: true
+    },
+    {
+        name: 'tvm_linker',
+        repoOwner: 'ton-actions',
+        repoName: 'build-tvm-linker',
+        assetPrefix: 'tvm_linker',
+        addToPath: true
+    }
+]
+
+async function getLatestReleaseInfo(repoOwner, repoName, assetPrefix){
+    const {data: latestRelease} = await octokit.repos.getLatestRelease({owner: repoOwner, repo: repoName});
+    let assertIndex = latestRelease.assets.findIndex(asset => asset.name.startsWith(assetPrefix));
+    if (assertIndex<0) {
+        throw `Asset was not found. Prefix:${assetPrefix} Release: ${latestRelease.url}`;        
+    }
+    return { tag: latestRelease.tag_name, assetDownloadUrl: latestRelease.assets[assertIndex].browser_download_url }
+}
+
+async function getTool(toolName, toolVersion, downloadUrl) {
+    // try get cached tools dir
+    let solcDir = tc.find(toolName, toolVersion);
+
+    // else download and cache tools
+    if (!solcDir) {
+        const archive = await tc.downloadTool(downloadUrl);
+        const extractedDir = await tc.extractTar(archive, `${toolsDir}/${toolName}`);
+        solcDir = await tc.cacheDir(extractedDir, toolName, toolVersion);
+    }
+    return solcDir;
+}
+
+async function run() {
+    try {
+        for (const tool of tools) {
+            core.info(`Install ${tool.name} Repo:${tool.repoOwner}/${tool.repoName}`)
+            
+            // get latest release and binary assert download url
+            const { tag: solcVersion, assetDownloadUrl: solcAssetDownloadUrl } = await getLatestReleaseInfo(tool.repoOwner, tool.repoName, tool.assetPrefix)
+            
+            // download or get tool from cache
+            const toolDir = await getTool(tool.name, solcVersion, solcAssetDownloadUrl);
+            
+            // add tool to path env variable
+            if (tool.addToPath) {
+                core.addPath(toolDir);
+            }
+            
+            // if need add assert with full path to env, for assert like TVM_LINKER_LIB_PATH
+            if (tool.addEnvVariable) {
+                core.exportVariable(tool.addEnvVariable, `${toolDir}/${tool.name}`)
+            }                       
+            
+            core.info(`Install ${tool.name} Repo:${tool.repoOwner}/${tool.repoName} Done.`)
+        }
+    } catch (error) {
+        core.setFailed(error.message);
+    }
+}
+
+run();    
